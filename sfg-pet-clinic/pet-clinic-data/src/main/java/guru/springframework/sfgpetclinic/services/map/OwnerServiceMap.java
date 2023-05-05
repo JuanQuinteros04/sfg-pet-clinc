58ed27849b9648a71cfe7ad65a8a491641ab0816
JuanQuinteros04 <juanmanuelquinteros@gmail.com>
2023-05-04 23:55:58 -0300

    adding bootstrap data.

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService    {
+    @Override
+    public Set<Owner> findall() {
+        return this.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        this.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return this.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
+}
