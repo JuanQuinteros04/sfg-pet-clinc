41a34b84404d987a581f41b656572279f6ef437f
JuanQuinteros04 <juanmanuelquinteros@gmail.com>
2023-05-04 23:55:58 -0300

    adding bootstrap data.

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
+    @Override
+    public Set<Vet> findall() {
+
+        return this.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+
+        return super.findById(id);
+    }
+}
